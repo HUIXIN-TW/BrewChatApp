9bca927227f33502093101e358ec11164108ff38
Yunchuan <leisyun@gmail.com>
2023-05-15 01:29:41 +0800

    adding pages and style

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -34,3 +34,103 @@ saveBtn.addEventListener("click", function() {
     elem.setAttribute("readonly", "");
   });
 });
+
+function type_effect_setting() {
+  // create a typing effect
+  var count = 0;
+  var speed = 100;
+  var idName = "type-effect";
+  var word = document.getElementById(idName).innerHTML;
+  typingEffect(word, idName, count, speed);
+}
+
+// creates a typing effect by gradually printing out the characters of a given word
+function typingEffect(word, idName, count, speed) {
+  // checks if the element exists in the DOM. If not, it returns.
+  const element = document.getElementById(idName);
+  if (!element) return;
+  // gets the substring of the word from the first character to the current count
+  const text = word.substring(0, count);
+  element.innerHTML = text;
+
+  // if the current count is less than the length of the word, it calls the function again
+  if (count < word.length) {
+    setTimeout(() => {
+      typingEffect(word, idName, count + 1, speed);
+    }, speed);
+  }
+}
+
+// load a random quote from an API
+function loadRandomQuote() {
+  var xhttp = new XMLHttpRequest();
+  const method = "GET";
+  const url = "https://api.quotable.io/random";
+  xhttp.open(method, url, true);
+
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var apiQuotes = JSON.parse(this.responseText);
+      var randomQuoteContent = apiQuotes.content;
+      var randomQuoteAuthor = apiQuotes.author || "- Anonymous";
+      var randomQuoteText =
+        '<span><i class="ion-quote"></i></span> &nbsp; ' + randomQuoteContent;
+      document.getElementById("quote").innerHTML = randomQuoteText;
+      document.getElementById("author").innerHTML = "- " + randomQuoteAuthor;
+    }
+  };
+  xhttp.send();
+}
+
+// Send message and show in the chat box
+function sendMessage() {
+  var messageInput = document.getElementById("message");
+  var messageText = messageInput.value;
+
+  if (messageText.trim() !== "") {
+      var chatWindow = document.getElementById("chat-window");
+      var newMessage = document.createElement("div");
+      newMessage.classList.add("message");
+      newMessage.classList.add("right");
+      newMessage.innerHTML = messageText;
+      chatWindow.appendChild(newMessage);
+      messageInput.value = "";
+  }
+}
+
+// using js and DOM to change the appearance of page
+function toggleMode() {
+  var icon = document.getElementById("mode-icon");
+  var currentClass = icon.className;
+
+  // if the current class is sunny, change it to moon and vice versa
+  if (currentClass === "ion-ios-sunny-outline") {
+    icon.className = "ion-ios-moon-outline";
+    document.body.classList.add("dark-mode");
+    document.querySelector("footer").classList.add("dark-mode");
+    document.querySelectorAll("nav li a").forEach(function (icon) {
+      icon.classList.add("dark-mode");
+    });
+  } else {
+    icon.className = "ion-ios-sunny-outline";
+    document.body.classList.remove("dark-mode");
+    document.querySelector("footer").classList.remove("dark-mode");
+    document.querySelectorAll("nav li a").forEach(function (icon) {
+      icon.classList.remove("dark-mode");
+    });
+  }
+}
+
+// mark the keyword in the message
+function mark() {
+  let keyWord = document.getElementById("keyword").value.trim();
+  if (keyWord !== "") {
+    let textList = document.querySelectorAll(".message.left, .message.right");
+    for (let i = 0; i < textList.length; i++) {
+      let text = textList[i].innerHTML;
+      let re = new RegExp(keyWord, "gi"); // create a regex, g=global match, i=case insensitive
+      let newText = text.replace(re, "<span class='searchHighlight'>$&</span>"); // replace the text
+      textList[i].innerHTML = newText;
+    }
+  }
+}
